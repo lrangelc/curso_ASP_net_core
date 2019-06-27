fe9f041c46a0c9eb0fdd2a904398a8f37f94baf0
Luis Rangel <luisrangelc@gmail.com>
2019-06-27 14:52:21 -0600

    Escuela VC

---
+++ HolaMundoMVC/Controllers/EscuelaController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaMundoMVC.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
