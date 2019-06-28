bd40ffbda318bf930599c56b32c630f6990e3c28
Luis Rangel <luisrangelc@gmail.com>
2019-06-27 22:47:29 -0600

    Vistas parciales

--- HolaMundoMVC/Controllers/AsignaturaController.cs
+++ HolaMundoMVC/Controllers/AsignaturaController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using HolaMundoMVC.Models;
+using System.Collections.Generic;
 
 namespace HolaMundoMVC.Controllers
 {
@@ -7,13 +8,25 @@ namespace HolaMundoMVC.Controllers
     {
         public IActionResult Index()
         {
-            var asignatura = new Asignatura{Nombre="Programacion"};
-            // escuela.UniqueId = Guid.NewGuid().ToString();
-            
+            var asignatura = new Asignatura { Nombre = "Programacion" };
+
             ViewBag.CosaDinamica = "La Monja";
-            ViewBag.Fecha = System.DateTime.Now;
 
             return View(asignatura);
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas"} ,
+                            new Asignatura{Nombre="Educación Física"},
+                            new Asignatura{Nombre="Castellano"},
+                            new Asignatura{Nombre="Ciencias Naturales"},
+                            new Asignatura{Nombre="Programacion"}
+                };
+
+            // return View("MultiAsignatura",asignatura);
+            return View(listaAsignaturas);
         }
     }
 }
