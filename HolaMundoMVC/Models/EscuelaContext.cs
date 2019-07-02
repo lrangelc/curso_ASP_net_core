ef37a8c3cedaccd1bc41ef803deddc18431f6846
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-02 00:35:17 -0600

    Refactoring siembra de datos

--- HolaMundoMVC/Models/EscuelaContext.cs
+++ HolaMundoMVC/Models/EscuelaContext.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -31,21 +32,84 @@ namespace HolaMundoMVC.Models
             escuela.Dirección = "5ta Calle";
             escuela.Ciudad = "Tecpan Guatemala";
             escuela.Pais = "Guatemala";
+
+            //Cargar Cursos de la Escuela
+            var cursos = CargarCursos(escuela);
+
+            //x cada curso cargar asignaturas
+            var asignaturas = CargarAsignaturas(cursos);
+
+            //x cada curso cargar alumnos
+            var alumnos = CargarAlumnos(cursos);
+
             modelBuilder.Entity<Escuela>().HasData(escuela);
+            modelBuilder.Entity<Curso>().HasData(cursos.ToArray());
+            modelBuilder.Entity<Asignatura>().HasData(asignaturas.ToArray());
+            modelBuilder.Entity<Alumno>().HasData(alumnos.ToArray());
 
-            var listaAsignaturas = new List<Asignatura>(){
-                            new Asignatura{Nombre="Matemáticas"} ,
-                            new Asignatura{Nombre="Educación Física"},
-                            new Asignatura{Nombre="Castellano"},
-                            new Asignatura{Nombre="Ciencias Naturales"},
-                            new Asignatura{Nombre="Programacion"}
-                };
-            modelBuilder.Entity<Asignatura>().HasData(listaAsignaturas);
+            // var listaAsignaturas = new List<Asignatura>(){
+            //                 new Asignatura{Nombre="Matemáticas"} ,
+            //                 new Asignatura{Nombre="Educación Física"},
+            //                 new Asignatura{Nombre="Castellano"},
+            //                 new Asignatura{Nombre="Ciencias Naturales"},
+            //                 new Asignatura{Nombre="Programacion"}
+            //     };
+            // modelBuilder.Entity<Asignatura>().HasData(listaAsignaturas);
 
-            modelBuilder.Entity<Alumno>().HasData(GenerarAlumnosAlAzar(5).ToArray());
+            // modelBuilder.Entity<Alumno>().HasData(GenerarAlumnosAlAzar(5).ToArray());
         }
 
-        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
+        private List<Alumno> CargarAlumnos(List<Curso> cursos)
+        {
+            var listaAlumnos = new List<Alumno>();
+
+            Random rnd = new Random();
+            foreach (var curso in cursos)
+            {
+                int cantRandom = rnd.Next(5, 20);
+                var tmplist = GenerarAlumnosAlAzar(curso, cantRandom);
+                listaAlumnos.AddRange(tmplist);
+            }
+            return listaAlumnos;
+        }
+
+        private static List<Asignatura> CargarAsignaturas(List<Curso> cursos)
+        {
+            var listaCompleta = new List<Asignatura> ();
+            foreach (var curso in cursos)
+            {
+                var tmpList = new List<Asignatura> {
+                            new Asignatura{
+                                CursoId = curso.Id,
+                                Nombre="Matemáticas"} ,
+                            new Asignatura{ CursoId = curso.Id, Nombre="Educación Física"},
+                            new Asignatura{ CursoId = curso.Id, Nombre="Castellano"},
+                            new Asignatura{ CursoId = curso.Id, Nombre="Ciencias Naturales"},
+                            new Asignatura{ CursoId = curso.Id, Nombre="Programación"}
+
+                };
+                listaCompleta.AddRange(tmpList);
+                //curso.Asignaturas = tmpList;
+            }
+
+            return listaCompleta;
+        }
+
+        private static List<Curso> CargarCursos(Escuela escuela)
+        {
+            return new List<Curso>(){
+                        new Curso{
+                            EscuelaId = escuela.Id,
+                            Nombre = "101",
+                            Jornada = TiposJornada.Mañana },
+                        new Curso{EscuelaId = escuela.Id, Nombre = "201", Jornada = TiposJornada.Mañana},
+                        new Curso{EscuelaId = escuela.Id, Nombre = "301", Jornada = TiposJornada.Mañana},
+                        new Curso{EscuelaId = escuela.Id, Nombre = "401", Jornada = TiposJornada.Tarde},
+                        new Curso{EscuelaId = escuela.Id, Nombre = "501", Jornada = TiposJornada.Tarde},
+            };
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar(Curso curso, int cantidad)
         {
             string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
             string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
@@ -54,7 +118,8 @@ namespace HolaMundoMVC.Models
             var listaAlumnos = from n1 in nombre1
                                from n2 in nombre2
                                from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+                               select new Alumno { CursoId = curso.Id,
+                                Nombre = $"{n1} {n2} {a1}" };
 
             return listaAlumnos.OrderBy((al) => al.Id).Take(cantidad).ToList();
         }
