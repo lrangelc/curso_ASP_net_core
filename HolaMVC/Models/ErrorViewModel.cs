79a4c0552e1443f33d95d64eb2f0ca77cc182192
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-07 17:37:46 -0600

    prueba

---
+++ HolaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HolaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
